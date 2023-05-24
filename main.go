91bb837892fcb4f1de655cce9ff1d837c25cd454
Chris Williamson <chriswill0629@gmail.com>
2023-05-23 22:21:44 -0400

    testing packages

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "log"
+import (
+	"github.com/cwilliamson29/myniceprogram/helpers"
+	"log"
+)
 
 func main() {
 	log.Println("Hello")
+
+	var myVar helpers.SomeType
+
+	myVar.TypeName = "Some name"
+
+	log.Println(myVar.TypeName)
 }
